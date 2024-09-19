feaaba5085df97c42e8b2ae88b8027271f1fa2cd
Yi <0418yicheng@gmail.com>
2024-09-19 16:31:29 -0400

    Added answers to Q1 and Q2 week 3

--- src/Week3/Week3Q1.java
+++ src/Week3/Week3Q1.java
@@ -6,6 +6,10 @@ public class Week3Q1{
         System.out.println(countWords("Congrats on winning Merrimack!"));
         System.out.println(countWords("Hi Ben."));
 
+        System.out.println(altSolution("The Quick Brown Fox Jumped Over the Lazy Dog"));
+        System.out.println(altSolution("Congrats on winning Merrimack!"));
+        System.out.println(altSolution("Hi Ben."));
+
         /*
          * Expected Output:
          *  9
@@ -19,5 +23,20 @@ public class Week3Q1{
         //TODO: Complete the function!
         // *HINT: [String].charAt(index) will give you the character at the specified index
         //           Remember that the first element is at index 0
+
+        int count = 0;
+        for(int i = 0; i < sentence.length(); i++){
+            if(sentence.charAt(i) == ' '){
+                count ++;
+            }
+        }
+        count ++;   //Accounts for the last word
+
+        return count;
+    }
+
+    public static int altSolution(String sentence){
+        String[] temp = sentence.split(" ");
+        return temp.length;
     }
 }
--- src/Week3/Week3Q2.java
+++ src/Week3/Week3Q2.java
@@ -2,10 +2,11 @@ package Week3;
 
 public class Week3Q2 {
     public static void main(String[] args){
-        System.out.println(getArea(3, 3, 3, 7, 5, 5));
-        System.out.println(getArea(7, 11, 2, 2, 4, 13));
-        System.out.println(getArea(-3, -3, -3, -6, -7, -6));
+        System.out.println(round(getArea(3, 3, 3, 7, 5, 5)));
+        System.out.println(round(getArea(7, 11, 2, 2, 4, 13)));
+        System.out.println(round(getArea(-3, -3, -3, -6, -7, -6)));
         
+
         /*
          * Expected Outputs:
          *  4
@@ -17,6 +18,31 @@ public class Week3Q2 {
     //Create a function that returns the area of the resulting triangle.
     public static double getArea(double x1, double y1, double x2, double y2, double x3, double y3){
         //TODO: Complete the function!
+        //A = sqrt(S(S-A)(S-B)(S-C))
+
+        double a = Math.hypot((x2-x1), (y2-y1));
+        double b = Math.hypot((x3-x2), (y3-y2));
+        double c = Math.hypot((x3-x1), (y3-y1));
+
+        double s = (a + b + c) / 2;
+
+        double area = Math.sqrt(s * (s-a) * (s-b) * (s-c));
+
+        return area;
+    }
+
+    // Sasha's Rounding Complaint
+    public static double round(double i){
+        //39.9
+        i *= 100;
+        int n = (int) i;
+
+        i = (double)n / 10;
+
+        i += 0.5;
+        i = (int) i;
+
+        return i/10;
     }
     
 }
