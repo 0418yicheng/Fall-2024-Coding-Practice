fad604960a4ac38232cbca2dab0a1aaa6a7d532f
Eshaan <superdog1155@gmail.com>
2024-09-19 16:35:46 -0400

    finsihed the two problems

--- src/Week3/Week3Q1.java
+++ src/Week3/Week3Q1.java
@@ -1,23 +1,28 @@
 package Week3;
 
-public class Week3Q1{
-    public static void main(){
+public class Week3Q1 {
+    public static void main(String[] args) {
+
         System.out.println(countWords("The Quick Brown Fox Jumped Over the Lazy Dog"));
         System.out.println(countWords("Congrats on winning Merrimack!"));
         System.out.println("Hi Ben.");
 
         /*
          * Expected Output:
-         *  9
-         *  4
-         *  2
+         * 9
+         * 4
+         * 2
          */
     }
 
-    // Create a function that counts the number of words in a sentence  
-    public static int countWords(String sentence){
-        //TODO: Complete the function!
-        // *HINT: [String].charAt(index) will give you the character at the specified index
-        //           Remember that the first element is at index 0
+    public static int countWords(String sentence) {
+        int wordCount = 0;
+        for (int i = 0; i < sentence.length(); i++) {
+            if (sentence.charAt(i) == ' ') {
+
+                wordCount++;
+            }
+        }
+        return wordCount + 1;
     }
 }
--- src/Week3/Week3Q2.java
+++ src/Week3/Week3Q2.java
@@ -1,22 +1,27 @@
 package Week3;
 
 public class Week3Q2 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println(getArea(3, 3, 3, 7, 5, 5));
         System.out.println(getArea(7, 11, 2, 2, 4, 13));
         System.out.println(getArea(-3, -3, -3, -6, -7, -6));
-        
+
         /*
          * Expected Outputs:
-         *  4
-         *  18.5
-         *  6
+         * 4
+         * 18.5
+         * 6
          */
     }
 
-    //Create a function that returns the area of the resulting triangle.
-    public static double getArea(double x1, double y1, double x2, double y2, double x3, double y3){
-        //TODO: Complete the function!
+    // Create a function that returns the area of the resulting triangle.
+    public static double getArea(double x1, double y1, double x2, double y2, double x3, double y3) {
+        double a = Math.hypot((x2 - x1), (y2 - y1));
+        double b = Math.hypot((x3 - x1), (y3 - y1));
+        double c = Math.hypot((x3 - x2), (y3 - y2));
+        double s = (a + b + c) / 2;
+        double answer = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+        return answer;
     }
-    
+
 }
