c43fbabee6a63e6317870e982764893e79f3421a
Yi <0418yicheng@gmail.com>
2024-09-19 15:56:02 -0400

    Fixed main bug

--- src/Week3/Week3Q1.java
+++ src/Week3/Week3Q1.java
@@ -1,7 +1,7 @@
 package Week3;
 
 public class Week3Q1{
-    public static void main(){
+    public static void main(String[] args){
         System.out.println(countWords("The Quick Brown Fox Jumped Over the Lazy Dog"));
         System.out.println(countWords("Congrats on winning Merrimack!"));
         System.out.println(countWords("Hi Ben."));
@@ -19,5 +19,6 @@ public class Week3Q1{
         //TODO: Complete the function!
         // *HINT: [String].charAt(index) will give you the character at the specified index
         //           Remember that the first element is at index 0
+        
     }
 }
