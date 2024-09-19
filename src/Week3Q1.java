1e8a0b8ca213f17c86f16a49f26b9cd6615611ea
Yi <0418yicheng@gmail.com>
2024-09-19 10:07:47 -0400

    first commit

---
+++ src/Week3Q1.java
@@ -0,0 +1,19 @@
+public class Week3Q1{
+    public static void main(){
+        System.out.println(countWords("The Quick Brown Fox Jumped Over the Lazy Dog"));
+        System.out.println(countWords("Congrats on winning Merrimack!"));
+        System.out.println("Hi Ben.");
+
+        /*
+         * Expected Output:
+         *  9
+         *  4
+         *  2
+         */
+    }
+
+    // Create a function that counts the number of words in a sentence  
+    public static int countWords(String sentence){
+        //TODO: Complete the function!
+    }
+}
