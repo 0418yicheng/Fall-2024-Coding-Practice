1d56df7eadbd540c79299543dcff7bf2c9530a72
Eshaan <superdog1155@gmail.com>
2024-09-26 15:51:44 -0400

    week 4

--- src/Week4/Week4Q1.java
+++ src/Week4/Week4Q1.java
@@ -1,34 +1,39 @@
 package Week4;
 
 public class Week4Q1 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         /*
          * Create an instance of the Student class with the following parameters
-         *      1) Name is Silly Ben
-         *      2) Age is 22
-         *      3) Gpa is -0.2
+         * 1) Name is Silly Ben
+         * 2) Age is 22
+         * 3) Gpa is -0.2
          */
 
+        // Now, print out Silly Ben's name, age, and gpa. They should be the values
+        // indicated above
 
-         // Now, print out Silly Ben's name, age, and gpa. They should be the values indicated above
-
-         //Make Silly Ben take a test.
+        // Make Silly Ben take a test.
+        Student s = new Student("Silly Ben", 22, -0.2);
+        System.out.println(s.name);
+        System.out.println(s.age);
+        System.out.println(s.gpa);
+        s.takeTest();
 
     }
 
-    //Fix any errors that appear.
-    public static class Student{
+    // Fix any errors that appear.
+    public static class Student {
         String name;
         int age;
         double gpa;
 
-        public Student(String name, int age, double gag){
+        public Student(String name, int age, double gag) {
             this.name = name;
-            age = age;
-            this.gpa = gpa;
+            this.age = age;
+            this.gpa = gag;
         }
-        
-        public void takeTest(){
+
+        public void takeTest() {
             System.out.println("Awwww, you failed.");
             gpa -= 10000;
             System.out.println(name + "'s gpa is now: " + gpa);
--- src/Week4/Week4Q2.java
+++ src/Week4/Week4Q2.java
@@ -1,19 +1,46 @@
 package Week4;
 
 public class Week4Q2 {
-    public static void main(String[] args){
-        /*
-         *  DO NOT start here
-         *  Create an instance of a red toyota with 69 miles.
-         *  Drive it.
-         */
+    public static void main(String[] args) {
+        Color c = new Color(255, 0, 0);
+        Car ca = new Car("Mustang", c, 0);
+        ca.Drive("BOOM", ca.mileage, 9999999);
     }
 
     /*
      * START HERE:
      * Create a class Car. It should have:
-     *      1) Fields to store the car's brand, color, and mileage
-     *      2) A constructor that sets all the values to user-inputted ones
-     *      3) Make a function drive that prints a sound and increases the mileage
+     * 1) Fields to store the car's brand, color, and mileage
+     * 2) A constructor that sets all the values to user-inputted ones
+     * 3) Make a function drive that prints a sound and increases the mileage
      */
+    public static class Color {
+        int red;
+        int green;
+        int blue;
+
+        public Color(int red, int green, int blue) {
+            this.red = red;
+            this.blue = blue;
+            this.green = green;
+        }
+    }
+
+    public static class Car {
+        String brand;
+        Color color;
+        int mileage;
+
+        public Car(String brand, Color color, int mileage) {
+            this.brand = brand;
+            this.color = color;
+            this.mileage = mileage;
+
+        }
+
+        public void Drive(String sound, int mileage, int mileageIncrease) {
+            mileage += mileageIncrease;
+            System.out.println(sound);
+        }
+    }
 }
