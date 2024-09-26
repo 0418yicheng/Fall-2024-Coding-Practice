7bb830d7abc7c016cb284f7f49ba73222fb44d5f
Yi <0418yicheng@gmail.com>
2024-09-26 14:46:11 -0400

    Answers for Week 4 questions

--- src/Week4/Week4Q1.java
+++ src/Week4/Week4Q1.java
@@ -8,12 +8,13 @@ public class Week4Q1 {
          *      2) Age is 22
          *      3) Gpa is -0.2
          */
-
-
+        Student ben = new Student("Silly Ben", 22, -0.2);
          // Now, print out Silly Ben's name, age, and gpa. They should be the values indicated above
-
+        System.out.println(ben.name);
+        System.out.println(ben.age);
+        System.out.println(ben.gpa);
          //Make Silly Ben take a test.
-
+        ben.takeTest();
     }
 
     //Fix any errors that appear.
@@ -24,8 +25,8 @@ public class Week4Q1 {
 
         public Student(String name, int age, double gag){
             this.name = name;
-            age = age;
-            this.gpa = gpa;
+            this.age = age; //<-- Add this. to the beginning
+            this.gpa = gag; //<-- Either change this to gag or change the parameter name to gpa
         }
         
         public void takeTest(){
--- src/Week4/Week4Q2.java
+++ src/Week4/Week4Q2.java
@@ -7,6 +7,8 @@ public class Week4Q2 {
          *  Create an instance of a red toyota with 69 miles.
          *  Drive it.
          */
+        Car toyota = new Car("Toyota", "Red", 69);
+        toyota.drive();
     }
 
     /*
@@ -16,4 +18,20 @@ public class Week4Q2 {
      *      2) A constructor that sets all the values to user-inputted ones
      *      3) Make a function drive that prints a sound and increases the mileage
      */
+    public static class Car{
+        String brand;
+        String color;
+        int mileage;
+
+        public Car(String brand, String color, int mileage){
+            this.brand = brand;
+            this.color = color;
+            this.mileage = mileage;
+        }
+
+        public void drive(){
+            System.out.println("VROOOM!!");
+            mileage += 1000;
+        }
+    }
 }
